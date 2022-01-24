42419683a5b9ceaddfcf905455c9eae15de70378
Ludovit <siket.ludovit@gmail.com>
2022-01-24 11:27:31 +0100

    Sending mail.
    Function conditions_for_email().

--- clown.py
+++ clown.py
@@ -3,25 +3,42 @@ import sys
 
 
 def check_existence(address):
-    return
+    print('Checking connnection existence for', address)
 
 
-def try_connect(adress):
-    return
+def try_connect(address):
+    print('Try connect to address', address)
 
 
 def syntax():
-    print('python3 clown.py [ftps address]')
-    sys.exit()
+    print('Syntax: python3 clown.py [ftps address]')
+
+
+def send_mail(sender, receivers):
+    print('Sending mail to', receivers)
+
+
+def conditions_for_email(i):
+    # 5 only for testing purposes
+    if i > 5:
+        send_mail('x', 'y')
+        i = 0
 
 
 def main():
-    ip = sys.argv[1]
-    if True:
+    attempt = 0
+    try:
+        ip = sys.argv[1]
         check_existence(ip)
-        try_connect(ip)
-    else:
-        print('No ftps connection for ', ip)
+        if try_connect(ip) is True:
+            print('Connection established')
+        else:
+            attempt += 1
+            print('No ftps connection for ', ip)
+            conditions_for_email(attempt)
+            sys.exit()
+    except IndexError:
+        syntax()
         sys.exit()
 
 
